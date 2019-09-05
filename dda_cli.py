9542a8929fddd850596c1e66fc8e347c0dac75ff
rtmdv <rtmdv@protonmail.com>
2019-09-05 17:51:47 -0400

    update dda_cli to functional state

--- dda_cli.py
+++ dda_cli.py
@@ -15,13 +15,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 """CLI Draft Assistant for Dota 2"""
 
 import io
 import json
 import operator
 from typing import List, Dict, Tuple
+
 
 class Hero:
     """Hero class for storing and accessing hero information."""
@@ -32,8 +32,8 @@ class Hero:
     bad_against: List[str]
     works_well_with: List[str]
 
-    def __init__(self, name, aliases, good_against,
-                 bad_against, works_well_with):
+    def __init__(self, name, aliases, good_against, bad_against,
+                 works_well_with):
         self.name = name
         self.aliases = aliases
         self.good_against = good_against
@@ -50,68 +50,181 @@ class Hero:
             'works_well_with': self.works_well_with,
         }
 
+    def pretty_print(self):
+        """Return human readable string representation of hero info."""
+        return ('Name: ' + self.name + '\n'
+                'Aliases: ' + ', '.join(self.aliases) + '\n'
+                'Good against: ' + ', '.join(self.good_against) + '\n'
+                'Bad against: ' + ', '.join(self.bad_against) + '\n'
+                'Works well with: ' + ', '.join(self.works_well_with))
+
 
 class DraftEngine:
     """Back end engine for draft assistant."""
 
-    hero_data: Dict[str, Hero]
-    hero_alias_map: Dict[str, str]
-    hero_weights: Dict[str, int]
-    ally_picks: List[str]
-    enemy_picks: List[str]
+    _hero_data: Dict[str, Hero]
+    _hero_alias_map: Dict[str, str]
+    _hero_weights: Dict[str, int]
+    _ally_picks: List[str]
+    _enemy_picks: List[str]
+    _max_picks: int = 5
 
     def __init__(self, hero_data):
-        self.hero_data = {}
-        self.hero_alias_map = {}
-        # TODO: load in hero data and set up alias map
+        self._hero_data = {}
+        self._hero_alias_map = {}
+
+        for hero in hero_data["heroes"]:
+            self._hero_data[hero["name"]] = Hero(
+                hero["name"],
+                hero["aliases"],
+                hero["good_against"],
+                hero["bad_against"],
+                hero["works_well_with"],
+            )
+
+        for hero_name in self._hero_data:
+            # map each aliases to hero name
+            for hero_alias in self._hero_data[hero_name].aliases:
+                self._hero_alias_map[hero_alias] = hero_name
+
+            # add lowercase hero name as well in case not present
+            self._hero_alias_map[hero_name.lower()] = hero_name
+
+        # initialize other instance attributes for start of draft
         self.reset()
 
     def reset(self):
-        self.ally_picks = []
-        self.enemy_picks = []
-        self.hero_weights = {}
+        """Reset engine, clearing picks and resetting hero weights to 0."""
+        self._ally_picks = []
+        self._enemy_picks = []
+        self._hero_weights = {}
+
+        for hero_name in self._hero_data:
+            self._hero_weights[hero_name] = 0
 
     def resolve_hero_name(self, alias: str):
-        pass
+        """Check for lowercase alias in hero alias map.
 
-    def ally_hero_pick(self, hero_pick: str):
+        Return hero name from map if found, else return None.
+        """
+        alias = alias.lower()
+        if alias in self._hero_alias_map:
+            return self._hero_alias_map[alias]
+        return None
+
+    def ally_hero_pick(self, hero_pick: str) -> Tuple[str, bool]:
         """Process ally hero pick and update hero weights."""
-        return 0
 
-    def enemy_hero_pick(self, hero_pick: str):
+        if not hero_pick:
+            return 'Hero argument required', False
+
+        hero_name = self.resolve_hero_name(hero_pick)
+        if not hero_name:
+            return f'No hero name or alias found matching: {hero_pick}', False
+
+        if hero_name in self._ally_picks or hero_name in self._enemy_picks:
+            return f'Hero {hero_name} already picked', False
+
+        if len(self._ally_picks) >= self._max_picks:
+            return 'Max ally hero picks reached', False
+
+        # passed validation, process pick
+        self._ally_picks.append(hero_name)
+
+        # remove picked hero from weights pool
+        if hero_name in self._hero_weights:
+            del self._hero_weights[hero_name]
+
+        # increase weight of heroes ally pick works well with
+        for other_hero in self._hero_data[hero_name].works_well_with:
+            if other_hero in self._hero_weights:
+                self._hero_weights[other_hero] += 1
+
+        return f'Ally hero pick: {hero_name}, hero weights updated', True
+
+    def enemy_hero_pick(self, hero_pick: str) -> Tuple[str, bool]:
         """Process enemy hero pick and update hero weights."""
-        return 0
+
+        if not hero_pick:
+            return 'Hero argument required', False
+
+        hero_name = self.resolve_hero_name(hero_pick)
+        if not hero_name:
+            return f'No hero name or alias found matching {hero_pick}', False
+
+        if hero_name in self._ally_picks or hero_name in self._enemy_picks:
+            return f'Hero {hero_name} already picked', False
+
+        if len(self._enemy_picks) >= self._max_picks:
+            return 'Max enemy hero picks reached', False
+
+        # passed validation, process pick
+        self._enemy_picks.append(hero_name)
+
+        # remove picked hero from weights pool
+        if hero_name in self._hero_weights:
+            del self._hero_weights[hero_name]
+
+        # decrease weight of heroes enemy pick is good against
+        for other_hero in self._hero_data[hero_name].good_against:
+            if other_hero in self._hero_weights:
+                self._hero_weights[other_hero] -= 1
+
+        # increase weight of heroes enemy pick is bad against
+        for other_hero in self._hero_data[hero_name].bad_against:
+            if other_hero in self._hero_weights:
+                self._hero_weights[other_hero] += 1
+
+        return f'Enemy hero pick: {hero_name}, hero weights updated', True
 
     def hero_info(self, hero: str):
+        """Get and return pretty printed hero information."""
         # check existence
-        return self.hero_data[hero].dict()
+        if not hero:
+            return 'Hero argument required', False
+
+        hero_name = self.resolve_hero_name(hero)
+        if not hero_name:
+            return f'No hero name or alias found matching {hero}', False
+
+        return self._hero_data[hero_name].pretty_print(), True
 
     def sorted_hero_weights(self) -> List[Tuple[str, int]]:
-        """Sort hero weights dict by value and return list of tuples."""
-        sorted_weights = sorted(self.hero_weights.items(),
+        """Sort hero weights dict by value in descending order."""
+        sorted_weights = sorted(self._hero_weights.items(),
                                 key=operator.itemgetter(1))
-        return sorted_weights
+        return sorted_weights[::-1]
+
+    @property
+    def ally_picks(self):
+        """Return list of ally picks."""
+        return self._ally_picks
+
+    @property
+    def enemy_picks(self):
+        """Return list of enemy picks."""
+        return self._enemy_picks
 
 
 class DraftCLI:
     """Command line interface for draft engine."""
 
-    draft_engine: DraftEngine
-    prompt_str: str = "> "
+    _draft_engine: DraftEngine
+    _prompt_str: str = "> "
 
     def __init__(self, hero_data):
-        self.draft_engine = DraftEngine(hero_data)
+        self._draft_engine = DraftEngine(hero_data)
         print('Draft engine initialized')
 
-    def prompt(self):
+    def _prompt(self):
         """Prompt for user input and process command.
 
         Returns boolean for whether to continue prompting or exit.
         """
-        cmd = input(self.prompt_str)
-        return self.process_command(cmd)
+        cmd = input(self._prompt_str)
+        return self._process_command(cmd)
 
-    def process_command(self, cmd: str):
+    def _process_command(self, cmd: str):
         """Process command from prompt.
 
         Returns boolean for whether to continue prompting or exit.
@@ -127,54 +240,78 @@ class DraftCLI:
             cmd_params = ''
 
         if cmd_base in ('h', 'help'):
-            self.help()
+            self._help()
         elif cmd_base in ('q', 'quit'):
             print('Quitting')
             return False
         elif cmd_base in ('r', 'reset'):
-            self.draft_engine.reset()
+            self._draft_engine.reset()
             print('Reset draft engine')
+        elif cmd_base in ('s', 'status'):
+            self._display_hero_weights()
         elif cmd_base in ('a', 'ally'):
-            if cmd_params:
-                self.draft_engine.ally_hero_pick(cmd_params)
-            else:
-                print('Hero argument required')
-                self.help()
+            out, success = self._draft_engine.ally_hero_pick(cmd_params)
+            print(out)
+            if success:
+                self._display_hero_weights()
         elif cmd_base in ('e', 'enemy'):
-            if cmd_params:
-                self.draft_engine.enemy_hero_pick(cmd_params)
-            else:
-                print('Hero argument required')
-                self.help()
+            out, success = self._draft_engine.enemy_hero_pick(cmd_params)
+            print(out)
+            if success:
+                self._display_hero_weights()
         elif cmd_base in ('i', 'info'):
-            if cmd_params:
-                print(self.draft_engine.hero_info(cmd_params))
-            else:
-                print('Hero argument required')
-                self.help()
+            out, success = self._draft_engine.hero_info(cmd_params)
+            print(out)
         else:
             print('Invalid command')
-            self.help()
+            self._help()
 
         return True
 
-    def help(self):
+    @staticmethod
+    def _help():
         """Print out command help."""
         print("""Help:
-        h, help
-        q, quit
-        r, reset
-        a <hero>, ally <hero>
-        e <hero>, enemy <hero>
-        i <hero>, info <hero>""")
+        h, help                   show help output
+        q, quit                   quit program
+        r, reset                  reset picks and weights
+        s, status                 show status of picks and weights
+        a <hero>, ally <hero>     ally hero pick
+        e <hero>, enemy <hero>    enemy hero pick
+        i <hero>, info <hero>     show hero info""")
+
+    def _display_hero_weights(self):
+        """Print out subset of hero weights, showing best and worst picks."""
+        weights = self._draft_engine.sorted_hero_weights()
+        num_heroes = len(weights)
+
+        # default list 10 best heroes and 5 worst
+        # in the future, add commands for configuring values
+        num_best = 15
+        num_worst = 5
+        assert num_heroes >= num_best
+        assert num_heroes >= num_worst
+
+        print('Status:')
+        print('Enemy picks: ' + ', '.join(self._draft_engine.enemy_picks))
+        print('Ally picks: ' + ', '.join(self._draft_engine.ally_picks))
+
+        print(f'{num_best} best picks and {num_worst} worst picks:')
+        for i in range(0, num_best):
+            print(f'  {weights[i][1]}, {weights[i][0]}')
+
+        print('...')
+        for i in range(num_heroes - num_worst, num_heroes):
+            print(f'  {weights[i][1]}, {weights[i][0]}')
 
     def run(self):
         """Run CLI, looping prompt until exit."""
-        while self.prompt():
+        while self._prompt():
             pass
 
 
 def main():
+    """Main for running DDA CLI."""
     # TODO: add argument handling for different file name
     hero_data_file = 'hero_data.json'
     try:
@@ -186,6 +323,7 @@ def main():
 
     draft_cli = DraftCLI(hero_data)
     draft_cli.run()
+
 
 if __name__ == '__main__':
     main()
